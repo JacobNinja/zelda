2c4dfd395418cf3fad5242b639e4efa2aa8d765a
Jacob Richardson <jacob.ninja.dev@gmail.com>
2013-11-07 17:52:26 -0700

    toggle between obstacle and remove mode with space in ui builder

--- src/cljs/zelda/ui.cljs
+++ src/cljs/zelda/ui.cljs
@@ -1,12 +1,20 @@
 (ns zelda.ui
   (:require [zelda.window]
-            [cljs.core.async :refer [>! <! chan]])
+            [cljs.core.async :refer [>! <! chan]]
+            [goog.events.KeyHandler]
+            [goog.events.KeyCodes :as key-codes]
+            [clojure.set :refer [difference]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def mouse-chan (chan))
+(def keyboard-chan (chan))
+(def draw-chan (chan))
+
+(def event (atom :obstacle))
+
+(def keyboard (goog.events.KeyHandler. js/document))
 
 (defn- push-mouse [e]
-  (go (>! mouse-chan [(.-pageX e) (.-pageY e)])))
+  (go (>! draw-chan [(.-pageX e) (.-pageY e)])))
 
 (defn- mousedown [e]
   (set! (.-onmousemove zelda.window/canvas) #(push-mouse %)))
@@ -14,20 +22,32 @@
 (defn- mousemove [e]
   (push-mouse e))
 
-(defn- mouse-loop []
+(defn- react-to-key [e]
+  (let [key (.-keyCode e)]
+    (when (= key key-codes/SPACE)
+      (if (= @event :obstacle)
+        (reset! event :remove)
+        (reset! event :obstacle)))))
+
+(defn- draw-loop []
   (go
    (loop [obstacles #{}]
-     (let [window-coords (<! mouse-chan)
+     (let [window-coords (<! draw-chan)
            coords (map #(int (/ % @zelda.window/cell-size)) window-coords)
-           next-obstacles (conj obstacles coords)]
-       (zelda.window/fill-square coords zelda.window/obstacle-color)
+           next-obstacles (condp = @event
+                            :obstacle (conj obstacles coords)
+                            :remove (difference obstacles #{coords})
+                            obstacles)]
+       (zelda.window/fill-empty)
+       (zelda.window/fill next-obstacles zelda.window/obstacle-color)
        (.log js/console (.toString next-obstacles))
        (recur next-obstacles)))))
-
 
 (defn ^:export init []
   (zelda.window/init-window)
   (let [canvas zelda.window/canvas]
     (set! (.-onmousedown canvas) mousedown)
     (set! (.-onmouseup canvas) (fn [] (set! (.-onmousemove canvas) nil)))
-    (mouse-loop)))
+    (set! (.-onkeydown canvas) react-to-key)
+    (.setAttribute canvas "tabindex" 0) ; WTF CANVAS
+    (draw-loop)))
